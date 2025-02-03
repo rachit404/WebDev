75c97ed8c822e33a2969c8c507d1e36d7d83030a
rachit404 <doshirachit28@gmail.com>
2025-02-03 15:57:45 +0530

    utils code done

--- Chai_aur_backend/src/index.js
+++ Chai_aur_backend/src/index.js
@@ -6,7 +6,16 @@ import connectDB from "./db/index.js";
 dotenv.config({
   path: "./env",
 });
-connectDB();
+
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`⚙️ Server is Running at port: ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Mongo db connection failed !!! ", err);
+  });
 
 /*
 import express from "express";
