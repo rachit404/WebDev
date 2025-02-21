8d9bffa07b9480f5f5b08165e1f34ff9b3edbb88
rachit404 <doshirachit28@gmail.com>
2025-02-21 22:20:47 +0530

    all setup configured

--- Chai_aur_backend/src/utils/asyncHandler.js
+++ Chai_aur_backend/src/utils/asyncHandler.js
@@ -1,7 +1,7 @@
 //! Learn more about this
 
 const asyncHandler = (requestHandler) => {
-  (req, res, next) => {
+  return (req, res, next) => {
     Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
   };
 };
