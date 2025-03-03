f3ac08efac661a79edd35ed6020efd1a5786a99e
rachit404 <doshirachit28@gmail.com>
2025-03-03 23:39:59 +0530

    fix all register controller bugs

--- Chai_aur_backend/src/controllers/user.controller.js
+++ Chai_aur_backend/src/controllers/user.controller.js
@@ -16,16 +16,27 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 
   // check if user already exists: username, email
-  const existedUser = User.findOne({
+  const existedUser = await User.findOne({
     $or: [{ email }, { username }],
   });
   if (existedUser) {
     throw new ApiError(409, "User with email or username already exists");
   }
 
+  // console.log(req.files);
+
   // check for images, check for avatar
   const avatarLocalPath = req.files?.avatar[0]?.path;
-  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+  // const coverImageLocalPath = req.files?.coverImage[0]?.path;
+  let coverImageLocalPath;
+  if (
+    req.files &&
+    Array.isArray(req.files.coverImage) &&
+    req.files.coverImage.length > 0
+  ) {
+    coverImageLocalPath = req.files.coverImage[0].path;
+  }
+
   if (!avatarLocalPath) {
     throw new ApiError(400, "Avatar is required");
   }
