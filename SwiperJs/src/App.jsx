b96a63348064ecf205aabb7f903594ace86c117a
rachit404 <doshirachit28@gmail.com>
2025-06-25 22:55:41 +0530

    start swiperjs

---
+++ SwiperJs/src/App.jsx
@@ -0,0 +1,27 @@
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+
+export default () => {
+  return (
+    <div className="flex justify-center items-center h-screen p-10">
+      <Swiper className="mySwiper bg-amber-100 w-3/4 h-3/4 flex justify-center items-center">
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide>
+      </Swiper>
+    </div>
+  );
+};
